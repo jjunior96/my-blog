35fd7be36359c0cf21ee1276ed78f56a4517205c
Junior Alves <jjalves96@gmail.com>
2019-12-22 09:30:25 -0300

    Gatsby Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
 
 const AboutPage = () => (
-  <h1> About Page </h1>
+  <Layout>
+    <h1> About Page </h1>
+    <Link to="/">Go to home</Link>
+  </Layout>
 )
 
 export default AboutPage
