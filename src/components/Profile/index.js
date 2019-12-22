7049391e6c7970979f97bbfce941f3f4b8a84754
Junior Alves <jjalves96@gmail.com>
2019-12-22 09:54:09 -0300

    Profile Component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Junior Alves</h1>
+    <h2>Desenvolvedor Full-Stack</h2>
+    <p>Criador da WolfCode, escrevo sobre o mundo do desenvolvimento Web e Mobile</p>
+  </div>
+)
+
+export default Profile
