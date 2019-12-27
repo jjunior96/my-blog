ca97ab24eea03cc4303dde3f0a2bbeb7be1caa67
Junior Alves <jjalves96@gmail.com>
2019-12-27 10:20:30 -0300

    feat: PostItem component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,20 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import PostItem from '../components/PostItem'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem 
+      slug="/about/"
+      background= "red"
+      category= "Misc"
+      date= "27 de Dezembro de 2019"
+      timetoRead= "5"
+      title= "Meu primeiro Post em meu blog"
+      description= "Minha experiencia em criar meu próprio blog"
+    />
   </Layout>
 )
 
