9c231882c86cad90db31c5076bfdd5c14d67a916
Junior Alves <jjalves96@gmail.com>
2020-01-05 10:10:30 -0300

    feat: recommededPosts create

---
+++ src/components/RecommendedPost/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+import * as S from './style'
+
+const RecommnendedPosts = ({ next, previous }) => (
+  <S.RecommnendedWrapper>
+    {previous && (
+      <S.RecommnendedLink to={previous.fields.slug} className="previous">
+        {previous.frontmatter.title}
+      </S.RecommnendedLink>
+    )}
+
+    {next && (
+      <S.RecommnendedLink to={next.fields.slug} className="next">
+        {next.frontmatter.title}
+      </S.RecommnendedLink>
+    )}
+  </S.RecommnendedWrapper>
+)
+
+RecommnendedPosts.propTypes = {
+  next: propTypes.shape({
+    frontmatter: propTypes.shape({
+      title: propTypes.string.isRequired
+    }),
+    fields: propTypes.shape({
+      slug: propTypes.string.isRequired
+    }),
+  }),
+  previous: propTypes.shape({
+    frontmatter: propTypes.shape({
+      title: propTypes.string.isRequired
+    }),
+    fields: propTypes.shape({
+      slug: propTypes.string.isRequired
+    }),
+  })
+}
+
+export default RecommnendedPosts
