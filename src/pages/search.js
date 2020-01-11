c2ee9f20ead4c4f0415930bc8a616465a13b4231
Junior Alves <jjalves96@gmail.com>
2020-01-11 06:11:53 -0300

    feat: algolia config and search page created

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import Search from '../components/Search'
 
 const SearchPage = () => (
   <Layout>
     <SEO title="Search"/>
-    <h1> Search Page </h1>
+    <Search />
   </Layout>
 )
 
