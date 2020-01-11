c5054007e6bf4f960f9713844e255379e2f0cf20
Junior Alves <jjalves96@gmail.com>
2020-01-11 07:02:54 -0300

    feat: items config results search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
-const Search = () => <h1>Search</h1>
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Stats } from 'react-instantsearch-dom';
+
+import Hit from './Hit'
+import * as S from './styled'
+
+const algolia = {
+  appID: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME
+}
+
+const searchClient = algoliasearch(algolia.appID, algolia.searchOnlyApiKey);
+
+
+const Search = () => (
+  <S.SearchWrapper>
+    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+      <SearchBox autoFocus translations={{ placeholder: 'Pesquisar...'}}/>
+      <Stats translations={{ stats(nbHits, timeSpentMs) {
+        return `${nbHits} resultados econtrados em ${timeSpentMs}ms`
+      } }}/>
+      <Hits hitComponent={Hit}/>
+    </InstantSearch>
+  </S.SearchWrapper>
+)
 
 export default Search
